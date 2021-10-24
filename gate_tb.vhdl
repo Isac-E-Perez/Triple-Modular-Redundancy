9b9c22659d902bb3949ee2582b57e470cc8a91df
Isac E Perez <89553126+Isac-E-Perez@users.noreply.github.com>
2021-10-24 14:31:53 -0500

    Create gate_tb.vhdl

---
+++ gate_tb.vhdl
@@ -0,0 +1,22 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity gate_tb is 
+end gate_tb;
+  
+architecture behavioral of gate_tb is 
+  
+  -- Compnent Declaratoin for the Unit Under Test (UUT)
+  component gate
+  generic (N : integer := 8); -- I use generic when performing routines that performs a certain task on an input array of a generic size. Default value of 8.
+  port ( 
+    A, B, C : in std_logic_vector(N-1 downto 0);
+    f : out std_logic_vector(N-1 downto 0);
+    y : out std_logic
+       );
+  end component;
+
+-- Inputs
+signal A : std_logic_vector(N-1 downto 0) := (others => '0');
+  
+-- Outputs   
