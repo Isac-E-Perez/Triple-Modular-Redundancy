a7792162c2d2ced3728ee2a2749756b6663aa5bc
Isac E Perez <89553126+Isac-E-Perez@users.noreply.github.com>
2021-10-24 18:19:09 -0500

    Update gate_tb.vhdl

--- gate_tb.vhdl
+++ gate_tb.vhdl
@@ -2,13 +2,13 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity gate_tb is 
+  generic (N : integer := 8); 
 end gate_tb;
   
 architecture behavioral of gate_tb is 
   
   -- Compnent Declaratoin for the Unit Under Test (UUT)
-  component gate
-    generic (N : integer := 8);  
+  component gate 
     port ( 
       A, B, C : in std_logic_vector(N-1 downto 0);
       f : out std_logic_vector(N-1 downto 0);
