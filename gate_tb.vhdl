4ae6544948b9f77dbcb3044c8a9bf82854e71b9b
Isac E Perez <89553126+Isac-E-Perez@users.noreply.github.com>
2021-10-24 18:16:40 -0500

    Update gate_tb.vhdl

--- gate_tb.vhdl
+++ gate_tb.vhdl
@@ -41,7 +41,7 @@ begin
     A <= x"CE"; B <= x"45"; C <= x"30"; wait for 20 ns;
     A <= x"00"; B <= x"00"; C <= x"00"; wait for 20 ns;
      
-    assert false report "Reached end of test"
+    assert false report "Reached end of test";
     wait;
   end process;
 end behavioral;
